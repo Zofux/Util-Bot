79ebcb6edbd6547f1223f354fa8fff6647c4cf76
Zofux <70540447+Zofux@users.noreply.github.com>
2021-10-30 16:55:02 +0200

    Update

---
+++ src/events/guildMemberRemove.js
@@ -0,0 +1,17 @@
+const moment = require("moment")
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'guildMemberRemove',
+    async execute(member) {
+        if (member.guild.id === "892751160182730772") {
+            const logChannel = member.guild.channels.cache.get("896370391247896616")
+            let embed = new Discord.MessageEmbed()
+                .setDescription(`:inbox_tray: <@${member.user.id}> Left the server`)
+                .addField(`Account created`, `${moment(member.joinedAt).format('LT')} ${moment(member.joinedAt).format('LL')} ${moment(member.joinedAt).fromNow()}`)
+                .setColor('#ff7575')
+                .setAuthor(`${member.user.username}`, member.user.displayAvatarURL())
+            logChannel.send({ embeds: [embed] })
+        }
+    }
+}
