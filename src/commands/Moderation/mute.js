c33ca3228d26d4d9c120a145e4df9cd1c7fa2fc8
Zofux <70540447+Zofux@users.noreply.github.com>
2021-10-31 12:20:08 +0100

    Update

--- src/commands/Moderation/mute.js
+++ src/commands/Moderation/mute.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js')
+const ms = require("ms")
 
 module.exports = {
     data: new SlashCommandBuilder()
