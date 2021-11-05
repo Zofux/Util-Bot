0cf12484a6567bc2dd22042082d5a0b956057353
Zofux <70540447+Zofux@users.noreply.github.com>
2021-11-05 16:52:58 +0100

    Update

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -8,7 +8,7 @@ module.exports = {
             const logChannel = member.guild.channels.cache.get("896370391247896616")
             let embed = new Discord.MessageEmbed()
                 .setDescription(`:inbox_tray: <@${member.user.id}> Left the server`)
-                .addField(`Account created`, `${moment(member.joinedAt).format('LT')} ${moment(member.joinedAt).format('LL')} ${moment(member.joinedAt).fromNow()}`)
+                .addField(`Joined at`, `${moment(member.joinedAt).format('LT')} ${moment(member.joinedAt).format('LL')} ${moment(member.joinedAt).fromNow()}`)
                 .setColor('#ff7575')
                 .setAuthor(`${member.user.username}`, member.user.displayAvatarURL())
             logChannel.send({ embeds: [embed] })
