32c3b03572ac0a7a4b37d94b44e4e183a37bb739
Zofux <70540447+Zofux@users.noreply.github.com>
2021-11-14 21:11:31 +0100

    Update

--- src/events/message.js
+++ src/events/message.js
@@ -5,13 +5,16 @@ module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
         if (message.guild) return;
+        if (message.author.bot) return;
+        const member = client.guilds.cache.get("892751160182730772").members.get(message.author.id)
+        if (!member) return;
         const res = await db.findOne({ userId: message.author.id })
         if (!res) {
             const embed = new Discord.MessageEmbed()
                 .setDescription(`<:cross:896045962940780555> You currently don't have any active captchas. use \`/verfiy\` in <#909502038516326500> to get one`)
                 .setColor("#ff7575")
                 .setAuthor("No active captchas")
-            message.author.send({ embeds: [embed] })
+            member.send({ embeds: [embed] })
         } else if (res) {
             if (message.content !== res.code) {
                 await db.findOneAndDelete({ userId: message.author.id }).then(async () => {
@@ -19,16 +22,15 @@ module.exports = {
                         .setDescription(`<:cross:896045962940780555> You sent the worng captcha code, plase use \`/verfiy\` in <#909502038516326500> to get a new one.`)
                         .setColor("#ff7575")
                         .setAuthor("Wrong Code")
-                    message.author.send({ embeds: [embed] })
+                    member.send({ embeds: [embed] })
                 })
             } else {
-                const member = client.guilds.cache.get("892751160182730772").members.get(message.author.id)
                 member.roles.add("892756988335898634").then(() => {
                     const embed = new Discord.MessageEmbed()
                         .setDescription(`<:check:896045976039608320> You have been verified`)
                         .setColor("#5797ff")
                         .setAuthor("Thank you!")
-                    message.author.send({ embeds: [embed] })
+                    member.send({ embeds: [embed] })
                 })
             }
         }
