6437eac0a4aa7953e3e5b3659a5940fb34e542be
Zofux <70540447+Zofux@users.noreply.github.com>
2021-11-14 21:48:48 +0100

    Update

--- src/events/captcha/captcha.js
+++ src/events/captcha/captcha.js
@@ -34,7 +34,7 @@ module.exports = async (member, client) => {
                 .setColor("#00ff74")
                 .setImage("attachment://captcha.png")
             member.send({ embeds: [embed], files: [attachment] }).catch(() => {
-                client.channels.cache.get("909502038516326500").send({ content: `<@${member.user.id}> You need to enable your dm's for the verification`, ephemeral: true })
+                client.guilds.cache.get("892751160182730772").channels.cache.get("909502038516326500").send({ content: `<@${member.user.id}> You need to enable your dm's for the verification`, ephemeral: true })
             })
         })
     })
