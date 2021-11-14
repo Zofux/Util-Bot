102d726ea93a06d00201c149b4b628d2d1fab051
Zofux <70540447+Zofux@users.noreply.github.com>
2021-11-14 21:16:21 +0100

    Update

--- src/events/message.js
+++ src/events/message.js
@@ -6,6 +6,7 @@ module.exports = {
     async execute(message, client) {
         if (message.guild) return;
         if (message.author.bot) return;
+        console.log(message.author.id)
         const member = client.guilds.cache.get("892751160182730772").members.cache.get(message.author.id)
         if (!member) return;
         const res = await db.findOne({ userId: message.author.id })
@@ -14,7 +15,7 @@ module.exports = {
                 .setDescription(`<:cross:896045962940780555> You currently don't have any active captchas. use \`/verfiy\` in <#909502038516326500> to get one`)
                 .setColor("#ff7575")
                 .setAuthor("No active captchas")
-            member.send({ embeds: [embed] })
+            message.author.send({ embeds: [embed] })
         } else if (res) {
             if (message.content !== res.code) {
                 await db.findOneAndDelete({ userId: message.author.id }).then(async () => {
@@ -22,7 +23,7 @@ module.exports = {
                         .setDescription(`<:cross:896045962940780555> You sent the worng captcha code, plase use \`/verfiy\` in <#909502038516326500> to get a new one.`)
                         .setColor("#ff7575")
                         .setAuthor("Wrong Code")
-                    member.send({ embeds: [embed] })
+                    message.author.send({ embeds: [embed] })
                 })
             } else {
                 member.roles.add("892756988335898634").then(() => {
@@ -30,7 +31,7 @@ module.exports = {
                         .setDescription(`<:check:896045976039608320> You have been verified`)
                         .setColor("#5797ff")
                         .setAuthor("Thank you!")
-                    member.send({ embeds: [embed] })
+                    message.author.send({ embeds: [embed] })
                 })
             }
         }
