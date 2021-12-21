56fdbd9dca6b3e833ccdb647a53b0dde408bb9f3
Zofux <70540447+Zofux@users.noreply.github.com>
2021-12-21 16:18:29 +0100

    Update

--- src/events/ready,.js
+++ src/events/ready,.js
@@ -4,7 +4,6 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        client.user.setActivity(`on Cactus Craft`, { type: "PLAYING" })
         console.log("Ready!")
 
         await require('./intervals/mutes')(client)
