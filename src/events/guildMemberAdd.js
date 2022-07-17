2413198a85393dbc478f16ea484580b33f5bacd2
Zofux <70540447+Zofux@users.noreply.github.com>
2022-07-17 18:38:27 +0200

    Update

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -4,13 +4,59 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
-            member.roles.add("892756988335898634")
-            const logChannel = member.guild.channels.cache.get("896370391247896616")
-            let embed = new Discord.MessageEmbed()
-                .setDescription(`:inbox_tray: <@${member.user.id}> Joined the server`)
-                .addField(`Account created`, `${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`)
-                .setColor('#43d490')
-                .setAuthor(`${member.user.username}`, member.user.displayAvatarURL())
-            logChannel.send({ embeds: [embed] })
+        member.roles.add("892756988335898634")
+        const logChannel = member.guild.channels.cache.get("896370391247896616")
+        let embed = new Discord.MessageEmbed()
+            .setDescription(`:inbox_tray: <@${member.user.id}> Joined the server`)
+            .addField(`Account created`, `${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`)
+            .setColor('#43d490')
+            .setAuthor(`${member.user.username}`, member.user.displayAvatarURL())
+        logChannel.send({ embeds: [embed] })
+
+        var randomstring = require("randomstring");
+        const id = randomstring.generate(6)
+
+        const { Captcha } = require('captcha-canvas')
+        const captcha = new Captcha()
+        captcha.async = true;
+        captcha.addDecoy();
+        captcha.drawTrace();
+        captcha.drawCaptcha({ text: id });
+
+        const captchaAttachment = new Discord.MessageAttachment(
+            await captcha.png,
+            "captcha.png"
+        )
+
+        const db = require('../models/captchas')
+        const res = await db.findOne({ userId: user.id })
+        const unixTime = require('unix-time');
+        const date = new Date()
+        date.setHours(date.getHours() + 1)
+
+        if (res) {
+            return
+        } else if (!res) {
+            if (interaction.member.roles.cache.some(role => role.id === "892756988335898634")) {
+                return
+            }
+            new db({
+                userId: user.id,
+                expiers: date,
+                code: id
+            }).save().then(async () => {
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor("Welcome to our server!")
+                    .setDescription(`Please send the captcha code back to me (<@892754578162982922>).\n\n**Why**\nThis is to protect the server against targeted attacks using bots\n\n**Expiers**\n<t:${unixTime(date)}:R>\n\n**Your Captcha:**`)
+                    .setImage('attachment://captcha.png')
+                    .setColor('#43d490')
+                    .setFooter("NOTE: This is Case Sensitive (Made by Zofux)")
+
+                await member.user.send({
+                    files: [captchaAttachment],
+                    embeds: [embed]
+                })
+            })
+        }
     }
 }
