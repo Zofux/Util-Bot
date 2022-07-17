4d65dd890559bfa61c2332fea1c6568303630345
Zofux <70540447+Zofux@users.noreply.github.com>
2022-07-17 18:42:21 +0200

    Update

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -4,7 +4,6 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
-        member.roles.add("892756988335898634")
         const logChannel = member.guild.channels.cache.get("896370391247896616")
         let embed = new Discord.MessageEmbed()
             .setDescription(`:inbox_tray: <@${member.user.id}> Joined the server`)
