213bcb40932e6f146562aa2e5204c0f928ae1889
Zofux <70540447+Zofux@users.noreply.github.com>
2022-07-24 21:03:53 +0200

    Update

--- bot.js
+++ bot.js
@@ -14,7 +14,13 @@ const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith("
 
 const { Player } = require("discord-player")
 client.player = new Player(client)
-
+client.player.on("trackStart", (queue, track) => {
+    const embed = new MessageEmbed()
+    .setColor("#f23a3a")
+    .setAuthor("Now Playing")
+    .setDescription(`[${track.title}](${track.url}) by ${track.author} [${track.duration}]`)
+    queue.metadata.channel.send({ embeds: [embed] })
+})
 
 function run() {
     for (file of functions) {
@@ -27,4 +33,5 @@ function run() {
     require('./src/database')()
 }
 run();
+
 
