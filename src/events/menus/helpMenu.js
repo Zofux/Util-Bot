3b3981c7953624c68f23ffbf33f74500ad927b09
Zofux <70540447+Zofux@users.noreply.github.com>
2022-07-31 23:59:30 +0200

    Update

--- src/events/menus/helpMenu.js
+++ src/events/menus/helpMenu.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js')
+const config = require('../../../config.json')
+
 module.exports = async (interaction, client) => {
     if (interaction.customId === 'information') {
 
