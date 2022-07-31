4b667582e5c6ab5f593c8d58e049560344e65a6a
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-01 00:23:01 +0200

    Update

--- src/events/menus/helpMenu.js
+++ src/events/menus/helpMenu.js
@@ -5,54 +5,54 @@ module.exports = async (interaction, client) => {
     if (interaction.customId === 'select') {
         if (interaction.values.includes("information")) {
             const embed = new Discord.MessageEmbed()
-            .setAuthor(`${client.user.username} - Commands [Information]`, client.user.displayAvatarURL())
-            .setColor(config.MainHexColor)
-            .setDescription()
-            .setFooter("Made by Zofux")
+                .setAuthor(`${client.user.username} - Commands [Information]`, client.user.displayAvatarURL())
+                .setColor(config.MainHexColor)
+                .setDescription("Test")
+                .setFooter("Made by Zofux")
 
-        await interaction.update({ embeds: [embed]})
+            await interaction.update({ embeds: [embed] })
         }
 
         if (interaction.values.includes("moderation")) {
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(`${client.user.username} - Commands [Moderation]`, client.user.displayAvatarURL())
-            .setColor(config.MainHexColor)
-            .setDescription()
-            .setFooter("Made by Zofux")
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`${client.user.username} - Commands [Moderation]`, client.user.displayAvatarURL())
+                .setColor(config.MainHexColor)
+                .setDescription("Test")
+                .setFooter("Made by Zofux")
 
-        await interaction.editReply({ embeds: [embed], ephemeral: true })
+            await interaction.update({ embeds: [embed] })
+        }
+
+        if (interaction.values.includes("music")) {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`${client.user.username} - Commands [Musci]`, client.user.displayAvatarURL())
+                .setColor(config.MainHexColor)
+                .setDescription("Test")
+                .setFooter("Made by Zofux")
+
+            await interaction.update({ embeds: [embed] })
+        }
+
+        if (interaction.values.includes("voice_channels")) {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`${client.user.username} - Commands [Information]`, client.user.displayAvatarURL())
+                .setColor(config.MainHexColor)
+                .setDescription("Test")
+                .setFooter("Made by Zofux")
+
+            await interaction.update({ embeds: [embed] })
+        }
+
+        if (interaction.values.includes("suggestions")) {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`${client.user.username} - Commands [suggestions]`, client.user.displayAvatarURL())
+                .setColor(config.MainHexColor)
+                .setDescription("Test")
+                .setFooter("Made by Zofux")
+
+            await interaction.update({ embeds: [embed] })
+        }
     }
 
-    if (interaction.values.includes("music")) {
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(`${client.user.username} - Commands [Musci]`, client.user.displayAvatarURL())
-            .setColor(config.MainHexColor)
-            .setDescription()
-            .setFooter("Made by Zofux")
 
-        await interaction.editReply({ embeds: [embed], ephemeral: true })
-    }
-
-    if (interaction.values.includes("voice_channels")) {
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(`${client.user.username} - Commands [Information]`, client.user.displayAvatarURL())
-            .setColor(config.MainHexColor)
-            .setDescription()
-            .setFooter("Made by Zofux")
-
-        await interaction.editReply({ embeds: [embed], ephemeral: true })
-    }
-
-    if (interaction.values.includes("suggestions")) {
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(`${client.user.username} - Commands [suggestions]`, client.user.displayAvatarURL())
-            .setColor(config.MainHexColor)
-            .setDescription()
-            .setFooter("Made by Zofux")
-
-        await interaction.editReply({ embeds: [embed], ephemeral: true })
-    }
-    }
-
-   
 }
