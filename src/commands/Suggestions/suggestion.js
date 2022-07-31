3730c6952a3868a0325a260fbc118bc5db1eddc5
Zofux <70540447+Zofux@users.noreply.github.com>
2022-07-31 18:49:05 +0200

    Update

--- src/commands/Suggestions/suggestion.js
+++ src/commands/Suggestions/suggestion.js
@@ -20,10 +20,10 @@ module.exports = {
                     option.setName(`status`)
                         .setDescription('Reject or accept the suggestion')
                         .setRequired(true)
-                        .addChoices(
+                        .addChoices([
                             { name: 'Reject', value: 'reject' },
                             { name: 'Accept', value: 'accept' }
-                        )
+                        ])
                 )
                 .addStringOption(option =>
                     option.setname('reply')
