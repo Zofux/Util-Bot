4cd371aa9c3cecf7370ac0168b1001aade83325b
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-01 19:09:26 +0200

    Update

--- src/events/menus/helpMenu.js
+++ src/events/menus/helpMenu.js
@@ -12,10 +12,7 @@ module.exports = async (interaction, client) => {
                     "\n\n\`[]\` : **Optional Argument**\n\`<>\` : **Required Argument**\n\u200B"
                 )
                 .addFields([
-                    { name: "🙋‍♂️Help", value: " `/help` This command shows you a detailed descri ption of every command" },
-                    { name: "✅Verify", value: "`/verify` This command will help you to verify again if you fail you initial verification"},
-                    { name: "⚠️Warnings", value: "`/warnings [user]` This command will show all warnings a user has"},
-                    { name: "🏆Level", value: "`/level` This command will show your current level"}
+                    { name: "Information commands", value: " `/help` This command shows you a detailed descri ption of every command\n`/verify` This command will help you to verify again if you fail you initial verification\n`/warnings [user]` This command will show all warnings a user has\n`/level` This command will show your current level" },
                 ])
                 .setFooter("Made by Zofux")
 
@@ -31,12 +28,9 @@ module.exports = async (interaction, client) => {
                 "\n\n\`[]\` : **Optional Argument**\n\`<>\` : **Required Argument**\n\u200B"
             )
             .addFields([
-                { name: "🔨Ban", value: " `/ban <user> <reason>` Ban somone from the server" },
-                { name: "🦿Kick", value: "`/kick <user> <reason>` Kick someone from the server"},
-                { name: "🔇Mute", value: "`/mute <user> <time> <reason>` Prevent someone from talking in chat"},
-                { name: "🔈Unmute", value: "`/unmute <user <reason>` Make some able to talk in chat again"},
-                { name: "⚠️Warn", value: "`/warn <user> <reason>` Give someone a warning"},
-                { name: "🧹Clearwarns", value: "`/clearwarns <user>` Clear all warnings from a user"}
+                { 
+                    name: "Moderation Commands", 
+                    value: "`/ban <user> <reason>` Ban somone from the server\n`/kick <user> <reason>` Kick someone from the server\n`/mute <user> <time> <reason>` Prevent someone from talking in chat\n`/unmute <user <reason>` Make some able to talk in chat again\n`/warn <user> <reason>` Give someone a warning\n`/clearwarns <user>` Clear all warnings from a user" },
             ])
             .setFooter("Made by Zofux")
 
@@ -52,13 +46,7 @@ module.exports = async (interaction, client) => {
                 "\n\n\`[]\` : **Optional Argument**\n\`<>\` : **Required Argument**\n\u200B"
             )
             .addFields([
-                { name: "▶️Play", value: " `/play <query>` Play any song of your choise" },
-                { name: "🗑️Clear", value: "`/clear` Remove all songs from the que"},
-                { name: "⏯️Pause", value: "`/pause` Pause the current music"},
-                { name: "📦Que", value: "`/que [page]` Make some able to talk in chat again"},
-                { name: "⏯️Resume", value: "`/resume` Resume the music after it has been paused"},
-                { name: "🔀Shuffle", value: "`/shuffle` Shuffle the songs in the que"},
-                { name: "⏭️Skip", value: "`/skip` Skip to the next song in the que"}
+                { name: "Music Commands", value: "`/play <query>` Play any song of your choise\n`/clear` Remove all songs from the que\n`/pause` Pause the current music\n`/que [page]` Make some able to talk in chat again\n`/resume` Resume the music after it has been paused\n`/shuffle` Shuffle the songs in the que\n`/skip` Skip to the next song in the que" },
             ])
             .setFooter("Made by Zofux")
 
@@ -74,10 +62,7 @@ module.exports = async (interaction, client) => {
                 "\n\n\`[]\` : **Optional Argument**\n\`<>\` : **Required Argument**\n\u200B"
             )
             .addFields([
-                { name: "🔒Voice Lock", value: " `/voice lock` Lock/unlock your current voice channel" },
-                { name: "🔇Voice Mute", value: "`/voice mute <user>` Mute a given user in your voice channel"},
-                { name: "🦿Voice Kick", value: "`/voice kick <user>` Kick someone from your voice channel"},
-                { name: "🔨Voice Ban", value: "`/voice ban <user>` Ban someone from your voice channel"},
+                { name: "Voice Channel Commands", value: "`/voice lock` Lock/unlock your current voice channel\n`/voice mute <user>` Mute a given user in your voice channel\n`/voice kick <user>` Kick someone from your voice channel\n`/voice ban <user>` Ban someone from your voice channel" },
             ])
             .setFooter("Made by Zofux")
 
@@ -93,7 +78,7 @@ module.exports = async (interaction, client) => {
                 "\n\n\`[]\` : **Optional Argument**\n\`<>\` : **Required Argument**\n\u200B"
             )
             .addFields([
-                { name: "💬Suggestion Reply", value: " `/suggestion reply <id> <status> [reply]` Reply to suggestions in the suggestion channel" },
+                { name: "Suggestion Commands", value: "`/suggestion reply <id> <status> [reply]` Reply to suggestions in the suggestion channel" },
             ])
             .setFooter("Made by Zofux")
 
