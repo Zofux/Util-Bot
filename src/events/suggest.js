bf58c5a8957397b5b3c9fc01fc6f7bfa22fbda22
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-05 22:42:05 +0200

    Update

--- src/events/suggest.js
+++ src/events/suggest.js
@@ -1,36 +1,42 @@
 const Discord = require("discord.js")
 const db = require('../models/suggestions')
+const suggestionChannels = require('../models/suggestionChannels')
 const config = require("../../config.json")
 
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
         if (!message.guild || message.author.bot) return
-        if (!config.suggestionChannel) return
-        else if (message.channel.id == config.suggestionChannel) {
-            const randomstring = require(`randomstring`);
-            const id = randomstring.generate(7)
+        const res = await suggestionChannels.findOne({ guildId: message.guild.id })
 
-            let embed = new Discord.MessageEmbed()
-                .setDescription(`**Submitter**\n<@${message.author.id}>\n\n**Suggestion**\n${message.content}`)
-                .setColor(config.MainHexColor)
-                .setAuthor(message.author.username, message.author.displayAvatarURL())
-                .setThumbnail(message.author.displayAvatarURL())
-                .setFooter(`User ID: ${message.author.id} | sID: ${id}`)
-                .setTimestamp()
-            message.channel.send({ embeds: [embed] }).then(async msg => {
-                msg.react("👍").then(() => msg.react("👎").then(async () => {
-                    await message.delete()
-                    new db({
-                        userId: message.author.id,
-                        id: id,
-                        messageId: message.id,
-                        suggestion: message.content,
-                        status: "pending"
-                    }).save()
-                }))
-            })
+        if (!res) return
+        else if (res) {
+            if (message.channel.id == res.suggestionChannel) {
+                const randomstring = require(`randomstring`);
+                const id = randomstring.generate(7)
+
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`**Submitter**\n<@${message.author.id}>\n\n**Suggestion**\n${message.content}`)
+                    .setColor(config.MainHexColor)
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setThumbnail(message.author.displayAvatarURL())
+                    .setFooter(`User ID: ${message.author.id} | sID: ${id}`)
+                    .setTimestamp()
+                message.channel.send({ embeds: [embed] }).then(async msg => {
+                    msg.react("👍").then(() => msg.react("👎").then(async () => {
+                        await message.delete()
+                        new db({
+                            userId: message.author.id,
+                            id: id,
+                            messageId: message.id,
+                            suggestion: message.content,
+                            status: "pending"
+                        }).save()
+                    }))
+                })
+            }
         }
+
 
 
     }
