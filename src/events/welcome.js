9366753ec0a4cd9959b240fc99a6c99b203d10e6
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-06 14:52:34 +0200

    Update

---
+++ src/events/welcome.js
@@ -0,0 +1,26 @@
+const db = require('../models/welcome')
+const Discord = require('discord.js')
+const config = require("../../config.json")
+
+module.exports = {
+    name: 'guildMemberAdd',
+    async execute(member) {
+        const res = await db.findOne({ guildId: member.guild.id })
+        if (!res) return
+        else if (res) {
+            const channel = member.guild.channels.cache.get(res.channelId)
+            if (!channel) await db.findOneAndDelete({ guildId: member.guild.id })
+            if (res.embed === false) {
+                let message = res.content;
+                message = message.replaceAll(`{user.username}`, member.user.username)
+                message = message.replaceAll(`{user.tag}`, `<@${memer.user.id}>`)
+                message = message.replaceAll(`{user.discriminator}`, member.user.discriminator)
+                message = message.replaceAll(`{server.name}`, member.guild.name)
+                message = message.replaceAll(`{server.members}`, member.guild.memberCount)
+                channel.send({ content: message })
+            } else if (res.embed === true) {
+                
+            }
+        }
+    }
+}
