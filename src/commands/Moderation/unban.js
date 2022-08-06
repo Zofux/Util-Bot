c9726fc576628a5ad8df33159a1b9487e64a3864
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-06 16:40:33 +0200

    Update

--- src/commands/Moderation/unban.js
+++ src/commands/Moderation/unban.js
@@ -42,77 +42,79 @@ module.exports = {
         const id = randomstring.generate(7)
 
         try {
-            let user = await interaction.guild.members.unban(userId)
-            const db = require('../../models/infractions')
-            const res = await db.findOne({ guildId: interaction.guild.id, userId: user.id })
+            await interaction.guild.members.unban(userId).then(user => {
+                const db = require('../../models/infractions')
+                const res = await db.findOne({ guildId: interaction.guild.id, userId: user.id })
 
-            if (!res) {
-                new db({
-                    userId: user.id,
-                    guildId: interaction.guild.id,
-                    infractions: [
-                        { type: `unban`, date: unixTime(new Date()), reason: reason, id: id, moderator: `${interaction.user.username}#${interaction.user.discriminator}` }
-                    ]
-                }).save().then(async () => {
-                    const logEmbed = new Discord.MessageEmbed()
-                        .setColor(config.SuccessHexColor)
-                        .addFields([
-                            { name: 'User', value: `${user.username}#${user.discriminator} (<@${user.id}>)`, inline: true },
-                            { name: 'Moderator', value: `${interaction.user.username}#${interaction.user.discriminator}`, inline: true },
-                            { name: 'Reason', value: reason }
-                        ])
-                        .setAuthor(`Unban | ${user.username}#${user.discriminator}`)
-                        .setFooter(interaction.guild.name)
-                        .setTimestamp()
-                    if (doLog) {
-                        logChannel.send({ embeds: [logEmbed] });
+                if (!res) {
+                    new db({
+                        userId: user.id,
+                        guildId: interaction.guild.id,
+                        infractions: [
+                            { type: `unban`, date: unixTime(new Date()), reason: reason, id: id, moderator: `${interaction.user.username}#${interaction.user.discriminator}` }
+                        ]
+                    }).save().then(async () => {
+                        const logEmbed = new Discord.MessageEmbed()
+                            .setColor(config.SuccessHexColor)
+                            .addFields([
+                                { name: 'User', value: `${user.username}#${user.discriminator} (<@${user.id}>)`, inline: true },
+                                { name: 'Moderator', value: `${interaction.user.username}#${interaction.user.discriminator}`, inline: true },
+                                { name: 'Reason', value: reason }
+                            ])
+                            .setAuthor(`Unban | ${user.username}#${user.discriminator}`)
+                            .setFooter(interaction.guild.name)
+                            .setTimestamp()
+                        if (doLog) {
+                            logChannel.send({ embeds: [logEmbed] });
+                        }
+
+                        const embed = new Discord.MessageEmbed()
+                            .setDescription(`${config.checkEmoji} Successfully **Unbanned** <@${user.id}> with id \`${id}\``)
+                            .setColor(config.SuccessHexColor)
+                            .setAuthor(interaction.user.username, interaction.user.displayAvatarURL())
+                            .setFooter(interaction.guild.name)
+                            .setTimestamp()
+                        return await interaction.editReply({ embeds: [embed] })
+
+                    })
+                } else if (res) {
+                    const infraction = {
+                        type: `unban`, date: unixTime(new Date()), reason: reason, id: id, moderator: `${interaction.user.username}#${interaction.user.discriminator}`
                     }
+                    await db.findOneAndUpdate({
+                        guildId: interaction.guild.id, userId: user.id
+                    }, {
+                        $push: { infractions: infraction }
+                    }, {
+                        new: true,
+                        upsert: true
+                    }).then(async () => {
+                        const logEmbed = new Discord.MessageEmbed()
+                            .setColor(config.SuccessHexColor)
+                            .addFields([
+                                { name: 'User', value: `${user.username}#${user.discriminator} (<@${user.id}>)`, inline: true },
+                                { name: 'Moderator', value: `${interaction.user.username}#${interaction.user.discriminator}`, inline: true },
+                                { name: 'Reason', value: reason }
+                            ])
+                            .setAuthor(`Unban | ${user.username}#${user.discriminator}`)
+                            .setFooter(interaction.guild.name)
+                            .setTimestamp()
+                        if (doLog) {
+                            logChannel.send({ embeds: [logEmbed] });
+                        }
 
-                    const embed = new Discord.MessageEmbed()
-                        .setDescription(`${config.checkEmoji} Successfully **Unbanned** <@${user.id}> with id \`${id}\``)
-                        .setColor(config.SuccessHexColor)
-                        .setAuthor(interaction.user.username, interaction.user.displayAvatarURL())
-                        .setFooter(interaction.guild.name)
-                        .setTimestamp()
-                    await interaction.editReply({ embeds: [embed] })
+                        const embed = new Discord.MessageEmbed()
+                            .setDescription(`${config.checkEmoji} Successfully **Unbanned** <@${user.id}> with id \`${id}\``)
+                            .setColor(config.SuccessHexColor)
+                            .setAuthor(interaction.user.username, interaction.user.displayAvatarURL())
+                            .setFooter(interaction.guild.name)
+                            .setTimestamp()
+                        return await interaction.editReply({ embeds: [embed] })
 
-                })
-            } else if (res) {
-                const infraction = {
-                    type: `unban`, date: unixTime(new Date()), reason: reason, id: id, moderator: `${interaction.user.username}#${interaction.user.discriminator}`
+                    })
                 }
-                await db.findOneAndUpdate({
-                    guildId: interaction.guild.id, userId: user.id
-                }, {
-                    $push: { infractions: infraction }
-                }, {
-                    new: true,
-                    upsert: true
-                }).then(async () => {
-                    const logEmbed = new Discord.MessageEmbed()
-                        .setColor(config.SuccessHexColor)
-                        .addFields([
-                            { name: 'User', value: `${user.username}#${user.discriminator} (<@${user.id}>)`, inline: true },
-                            { name: 'Moderator', value: `${interaction.user.username}#${interaction.user.discriminator}`, inline: true },
-                            { name: 'Reason', value: reason }
-                        ])
-                        .setAuthor(`Unban | ${user.username}#${user.discriminator}`)
-                        .setFooter(interaction.guild.name)
-                        .setTimestamp()
-                    if (doLog) {
-                        logChannel.send({ embeds: [logEmbed] });
-                    }
+            })
 
-                    const embed = new Discord.MessageEmbed()
-                        .setDescription(`${config.checkEmoji} Successfully **Unbanned** <@${user.id}> with id \`${id}\``)
-                        .setColor(config.SuccessHexColor)
-                        .setAuthor(interaction.user.username, interaction.user.displayAvatarURL())
-                        .setFooter(interaction.guild.name)
-                        .setTimestamp()
-                    await interaction.editReply({ embeds: [embed] })
-
-                })
-            }
         } catch {
             const embed = new Discord.MessageEmbed()
                 .setDescription(`${config.crossEmoji} No user with the id: \`${userId}\` is banned in this server`)
