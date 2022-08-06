cf0af3eb624d6800319b9e4ee2abb731557821a9
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-06 15:20:49 +0200

    Update

--- src/events/welcome.js
+++ src/events/welcome.js
@@ -9,58 +9,62 @@ module.exports = {
         if (!res) return
         else if (res) {
             const channel = member.guild.channels.cache.get(res.channelId)
-            if (!channel) await db.findOneAndDelete({ guildId: member.guild.id })
-            if (res.embed === false) {
-                let message = res.content;
-                message = message.replaceAll(`{user.username}`, member.user.username)
-                message = message.replaceAll(`{user.tag}`, `<@${member.user.id}>`)
-                message = message.replaceAll(`{user.discriminator}`, member.user.discriminator)
-                message = message.replaceAll(`{server.name}`, member.guild.name)
-                message = message.replaceAll(`{server.members}`, member.guild.memberCount)
-                channel.send({ content: message })
-            } else if (res.embed === true) {
-                let embed = new Discord.MessageEmbed()
-                embed.setColor(res.color)
+            if (!channel) {
+                await db.findOneAndDelete({ guildId: member.guild.id })
+            } else if (channel) {
+                if (res.embed === false) {
+                    let message = res.content;
+                    message = message.replaceAll(`{user.username}`, member.user.username)
+                    message = message.replaceAll(`{user.tag}`, `<@${member.user.id}>`)
+                    message = message.replaceAll(`{user.discriminator}`, member.user.discriminator)
+                    message = message.replaceAll(`{server.name}`, member.guild.name)
+                    message = message.replaceAll(`{server.members}`, member.guild.memberCount)
+                    channel.send({ content: message })
+                } else if (res.embed === true) {
+                    let embed = new Discord.MessageEmbed()
+                    embed.setColor(res.color)
 
-                let description = res.content;
-                description = description.replaceAll(`{user.username}`, member.user.username)
-                description = description.replaceAll(`{user.tag}`, `<@${member.user.id}>`)
-                description = description.replaceAll(`{user.discriminator}`, member.user.discriminator)
-                description = description.replaceAll(`{server.name}`, member.guild.name)
-                description = description.replaceAll(`{server.members}`, member.guild.memberCount)
-                embed.setDescription(description)
-                if (res.timestamp) embed.setTimestamp()
+                    let description = res.content;
+                    description = description.replaceAll(`{user.username}`, member.user.username)
+                    description = description.replaceAll(`{user.tag}`, `<@${member.user.id}>`)
+                    description = description.replaceAll(`{user.discriminator}`, member.user.discriminator)
+                    description = description.replaceAll(`{server.name}`, member.guild.name)
+                    description = description.replaceAll(`{server.members}`, member.guild.memberCount)
+                    embed.setDescription(description)
+                    if (res.timestamp) embed.setTimestamp()
 
-                if (res.title) {
-                    let title = res.title;
-                    title = title.replaceAll(`{user.username}`, member.user.username)
-                    title = title.replaceAll(`{user.discriminator}`, member.user.discriminator)
-                    title = title.replaceAll(`{server.name}`, member.guild.name)
-                    title = title.replaceAll(`{server.members}`, member.guild.memberCount)
-                    embed.setAuthor(title)
+                    if (res.title) {
+                        let title = res.title;
+                        title = title.replaceAll(`{user.username}`, member.user.username)
+                        title = title.replaceAll(`{user.discriminator}`, member.user.discriminator)
+                        title = title.replaceAll(`{server.name}`, member.guild.name)
+                        title = title.replaceAll(`{server.members}`, member.guild.memberCount)
+                        embed.setAuthor(title)
+                    }
+
+                    if (res.footer) {
+                        let footer = res.footer;
+                        footer = footer.replaceAll(`{user.username}`, member.user.username)
+                        footer = footer.replaceAll(`{user.discriminator}`, member.user.discriminator)
+                        footer = footer.replaceAll(`{server.name}`, member.guild.name)
+                        footer = footer.replaceAll(`{server.members}`, member.guild.memberCount)
+                        embed.setFooter(footer)
+                    }
+
+                    if (res.image) {
+                        if (res.image === "{user.avatar}") embed.setImage(member.user.displayAvatarURL())
+                        if (res.image === "{server.icon}") embed.setImage(member.guild.iconURL())
+                    }
+
+                    if (res.thumbnail) {
+                        if (res.thumbnail === "{user.avatar}") embed.setThumbnail(member.user.displayAvatarURL())
+                        if (res.thumbnail === "{server.icon}") embed.setThumbnail(member.guild.iconURL())
+                    }
+
+                    await channel.send({ embeds: [embed] })
                 }
-
-                if (res.footer) {
-                    let footer = res.footer;
-                    footer = footer.replaceAll(`{user.username}`, member.user.username)
-                    footer = footer.replaceAll(`{user.discriminator}`, member.user.discriminator)
-                    footer = footer.replaceAll(`{server.name}`, member.guild.name)
-                    footer = footer.replaceAll(`{server.members}`, member.guild.memberCount)
-                    embed.setFooter(footer)
-                }
-
-                if (res.image) {
-                    if (res.image === "{user.avatar}") embed.setImage(member.user.displayAvatarURL())
-                    if (res.image === "{server.icon}") embed.setImage(member.guild.iconURL())
-                }
-
-                if (res.thumbnail) {
-                    if (res.thumbnail === "{user.avatar}") embed.setThumbnail(member.user.displayAvatarURL())
-                    if (res.thumbnail === "{server.icon}") embed.setThumbnail(member.guild.iconURL())
-                }
-
-                await channel.send({ embeds: [embed] })
             }
+
         }
     }
 }
