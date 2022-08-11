dba7110e7c5dd72f60ef7bea9ad713462c265dc0
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-11 22:18:14 +0200

    Update

--- src/events/modmail.js
+++ src/events/modmail.js
@@ -7,53 +7,77 @@ module.exports = {
     async execute(message, client) {
         if (message.author.bot) return;
 
-        const guild = client.guilds.cache.get(config.guild)
-        const channel = guild.channels.cache.find(c => c.name === message.author.id)
+        if (message.guild) {
+            const member = message.guild.members.cache.get(message.channel.name)
+            if (!member) {
+                const embed = new Discord.MessageEmbed()
+                    .setDescription(`${config.crossEmoji} That user isn't in this server`)
+                    .setColor(config.ErrorHexColor)
+                    .setAuthor(interaction.user.username, interaction.user.displayAvatarURL())
+                    .setFooter(interaction.guild.name)
+                    .setTimestamp()
+                return interaction.editReply({ embeds: [embed], ephemeral: true })
+            }
 
-        if (!channel) {
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(client.user.username, client.user.displayAvatarURL())
-                .setThumbnail(client.user.displayAvatarURL())
+            const mail = new Discord.MessageEmbed()
+                .setAuthor(message.author.username, message.author.displayAvatarURL())
+                .setThumbnail(message.author.displayAvatarURL())
                 .setColor(config.MainHexColor)
-                .setDescription("Thank you for your message, our staff team will be with you shortly. Please send any additional details that could issue")
-            message.author.send({ embeds: [embed] }).then(async () => {
-                client.guilds.cache.get(config.guild).channels.create(message.author.id, {
-                    type: 'GUILD_TEXT',
-                    parent: config.modMailCategoryId,
-                    permissionOverwrites: [
-                        {
-                            id: config.guild,
-                            deny: ["VIEW_CHANNEL"]
-                        },
-                        {
-                            id: config.moderatorRole,
-                            allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "SEND_MESSAGES"],
-                        }
-                    ]
-                }).then((mailChannel) => {
+                .setDescription(message.content)
+            member.send({ embeds: [mail] }).then(async () => {
+                await message.delete()
+            })
+
+        } else if (!message.guild) {
+            const guild = client.guilds.cache.get(config.guild)
+            const channel = guild.channels.cache.find(c => c.name === message.author.id)
+
+            if (!channel) {
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(client.user.username, client.user.displayAvatarURL())
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .setColor(config.MainHexColor)
+                    .setDescription("Thank you for your message, our staff team will be with you shortly. Please send any additional details that could issue")
+                message.author.send({ embeds: [embed] }).then(async () => {
+                    client.guilds.cache.get(config.guild).channels.create(message.author.id, {
+                        type: 'GUILD_TEXT',
+                        parent: config.modMailCategoryId,
+                        permissionOverwrites: [
+                            {
+                                id: config.guild,
+                                deny: ["VIEW_CHANNEL"]
+                            },
+                            {
+                                id: config.moderatorRole,
+                                allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "SEND_MESSAGES"],
+                            }
+                        ]
+                    }).then((mailChannel) => {
+                        const mail = new Discord.MessageEmbed()
+                            .setAuthor(message.author.username, message.author.displayAvatarURL())
+                            .setThumbnail(message.author.displayAvatarURL())
+                            .setColor(config.MainHexColor)
+                            .setDescription(message.content)
+                        mailChannel.send({ embeds: [mail] })
+                    })
+                })
+            } else if (channel) {
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(client.user.username, client.user.displayAvatarURL())
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .setColor(config.MainHexColor)
+                    .setDescription("Your message has been sent")
+                message.author.send({ embeds: [embed] }).then(() => {
                     const mail = new Discord.MessageEmbed()
                         .setAuthor(message.author.username, message.author.displayAvatarURL())
                         .setThumbnail(message.author.displayAvatarURL())
                         .setColor(config.MainHexColor)
                         .setDescription(message.content)
-                    mailChannel.send({ embeds: [mail] })
+                    channel.send({ embeds: [mail] })
                 })
-            })
-        } else if (channel) {
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(client.user.username, client.user.displayAvatarURL())
-                .setThumbnail(client.user.displayAvatarURL())
-                .setColor(config.MainHexColor)
-                .setDescription("Your message has been sent")
-            message.author.send({ embeds: [embed] }).then(() => {
-                const mail = new Discord.MessageEmbed()
-                    .setAuthor(message.author.username, message.author.displayAvatarURL())
-                    .setThumbnail(message.author.displayAvatarURL())
-                    .setColor(config.MainHexColor)
-                    .setDescription(message.content)
-                channel.send({ embeds: [mail] })
-            })
 
+            }
         }
+
     }
 }
