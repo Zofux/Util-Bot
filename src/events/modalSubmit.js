1c918a70fac38fe494bac614d86a8620a5db714d
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-11 19:26:46 +0200

    Update

--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -6,8 +6,77 @@ const config = require("../../config.json")
 module.exports = {
     name: 'modalSubmit',
     async execute(modal, client) {
+        function createChannel(id) {
+            client.guilds.cache.get(newState.guild.id).channels.create(`${id}-${modal.customId}`, {
+                type: 'GUILD_TEXT',
+                parent: config.applicationCategoryId,
+                permissionOverwrites: [
+                    {
+                        id: modal.guild.id,
+                        deny: ["VIEW_CHANNEL"]
+                    },
+                    {
+                        id: config.applicationsRole,
+                        allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "SEND_MESSAGES"],
+                    }
+                ]
+            })
+        }
+
         if (modal.customId === "staff") {
-            console.log(modal)
+
+            const id = Math.floor(Math.random() * 90000) + 10000;
+            const channel = createChannel(id)
+
+            const age = modal.getSelectMenyValues('age')
+            const timeZone = modal.getTextInputValue('time-zone')
+            const part = modal.getTextInputValue('part')
+            const goal = modal.getTextInputValue('goal')
+            const why = modal.getTextInputValue('why')
+
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`Info-${modal.user.username}`, modal.user.displayAvatarURL())
+                .setDescription(
+                    `**User:** <@${modal.user.id}>\`(${modal.user.id})\`` +
+                    `**Joined:** <t:${unixTime(modal.joinedTimestamp)}:R>`
+                    )
+                .setColor(config.MainHexColor)
+            const embed1 = new Discord.MessageEmbed()
+                .setColor(config.MainHexColor)
+                .setDescription(age)
+
+            const embed2 = new Discord.MessageEmbed()
+                .setColor(config.MainHexColor)
+                .setDescription(timeZone)
+
+            const embed3 = new Discord.MessageEmbed()
+                .setColor(config.MainHexColor)
+                .setDescription(part)
+
+            const embed4 = new Discord.MessageEmbed()
+                .setColor(config.MainHexColor)
+                .setDescription(goal)
+
+            const embed5 = new Discord.MessageEmbed()
+                .setColor(config.MainHexColor)
+                .setDescription(why)
+                .setTimestamp()
+
+                const Button = new Discord.MessageActionRow()
+                .addComponents(
+                    new Discord.MessageButton()
+                        .setCustomId("accept")
+                        .setLabel("Accept")
+                        .setStyle("PRIMARY")
+                )
+                .addComponents(
+                    new Discord.MessageButton()
+                        .setCustomId("deny")
+                        .setLabel("Deny")
+                        .setStyle("DANGER")
+                )
+
+            channel.send({ embeds: [embed, embed1, embed2, embed3, embed4, embed5], components: [Button] })
             modal.reply({ content: "Thx <3", ephemeral: true })
         }
     }
--- src/models/applications.js
+++ src/models/applications.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const model = new mongoose.Schema({
     userId: String,
     date: String,
-    id: String
+    id: String,
+    channelId: String
 })
 
 module.exports = mongoose.model('Applications', model)
