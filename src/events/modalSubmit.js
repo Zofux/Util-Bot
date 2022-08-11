34822cb079d252a5093bee3f75bdf83e262963c9
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-11 19:43:13 +0200

    Update

--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -6,7 +6,9 @@ const config = require("../../config.json")
 module.exports = {
     name: 'modalSubmit',
     async execute(modal, client) {
-        function createChannel(id) {
+
+        if (modal.customId === "staff") {
+            const id = Math.floor(Math.random() * 90000) + 10000;
             client.guilds.cache.get(modal.guild.id).channels.create(`${id}-${modal.customId}`, {
                 type: 'GUILD_TEXT',
                 parent: config.applicationCategoryId,
@@ -20,63 +22,58 @@ module.exports = {
                         allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "SEND_MESSAGES"],
                     }
                 ]
-            })
-        }
+            }).then((channel) => {
+                const age = modal.getSelectMenuValues('age')
+                const timeZone = modal.getTextInputValue('time-zone')
+                const part = modal.getTextInputValue('part')
+                const goal = modal.getTextInputValue('goal')
+                const why = modal.getTextInputValue('why')
 
-        if (modal.customId === "staff") {
-            const id = Math.floor(Math.random() * 90000) + 10000;
-            const channel = createChannel(id)
-
-            const age = modal.getSelectMenuValues('age')
-            const timeZone = modal.getTextInputValue('time-zone')
-            const part = modal.getTextInputValue('part')
-            const goal = modal.getTextInputValue('goal')
-            const why = modal.getTextInputValue('why')
-
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(`Info-${modal.user.username}`, modal.user.displayAvatarURL())
-                .setDescription(
-                    `**User:** <@${modal.user.id}>\`(${modal.user.id})\`` +
-                    `**Joined:** <t:${unixTime(modal.joinedTimestamp)}:R>`
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(`Info-${modal.user.username}`, modal.user.displayAvatarURL())
+                    .setDescription(
+                        `**User:** <@${modal.user.id}>\`(${modal.user.id})\`` +
+                        `**Joined:** <t:${unixTime(modal.joinedTimestamp)}:R>`
                     )
-                .setColor(config.MainHexColor)
-            const embed1 = new Discord.MessageEmbed()
-                .setColor(config.MainHexColor)
-                .setDescription(age[0])
+                    .setColor(config.MainHexColor)
+                const embed1 = new Discord.MessageEmbed()
+                    .setColor(config.MainHexColor)
+                    .setDescription(age[0])
 
-            const embed2 = new Discord.MessageEmbed()
-                .setColor(config.MainHexColor)
-                .setDescription(timeZone)
+                const embed2 = new Discord.MessageEmbed()
+                    .setColor(config.MainHexColor)
+                    .setDescription(timeZone)
 
-            const embed3 = new Discord.MessageEmbed()
-                .setColor(config.MainHexColor)
-                .setDescription(part)
+                const embed3 = new Discord.MessageEmbed()
+                    .setColor(config.MainHexColor)
+                    .setDescription(part)
 
-            const embed4 = new Discord.MessageEmbed()
-                .setColor(config.MainHexColor)
-                .setDescription(goal)
+                const embed4 = new Discord.MessageEmbed()
+                    .setColor(config.MainHexColor)
+                    .setDescription(goal)
 
-            const embed5 = new Discord.MessageEmbed()
-                .setColor(config.MainHexColor)
-                .setDescription(why)
-                .setTimestamp()
+                const embed5 = new Discord.MessageEmbed()
+                    .setColor(config.MainHexColor)
+                    .setDescription(why)
+                    .setTimestamp()
 
                 const Button = new Discord.MessageActionRow()
-                .addComponents(
-                    new Discord.MessageButton()
-                        .setCustomId("accept")
-                        .setLabel("Accept")
-                        .setStyle("PRIMARY")
-                )
-                .addComponents(
-                    new Discord.MessageButton()
-                        .setCustomId("deny")
-                        .setLabel("Deny")
-                        .setStyle("DANGER")
-                )
+                    .addComponents(
+                        new Discord.MessageButton()
+                            .setCustomId("accept")
+                            .setLabel("Accept")
+                            .setStyle("PRIMARY")
+                    )
+                    .addComponents(
+                        new Discord.MessageButton()
+                            .setCustomId("deny")
+                            .setLabel("Deny")
+                            .setStyle("DANGER")
+                    )
 
-            channel.send({ embeds: [embed, embed1, embed2, embed3, embed4, embed5], components: [Button] })
-            modal.reply({ content: "Thx <3", ephemeral: true })
+                channel.send({ embeds: [embed, embed1, embed2, embed3, embed4, embed5], components: [Button] })
+                modal.reply({ content: "Thx <3", ephemeral: true })
+            })
         }
     }
 }
