09977e5dea53cddb0919c8c97b0d6fb17e877f00
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-11 17:37:27 +0200

    Update

--- src/events/ready.js
+++ src/events/ready.js
@@ -16,7 +16,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setAuthor(guild.name, guild.iconURL())
             .setDescription(
-                "Hey there new member, before you join our server I would like to go through some basic stuff with you." +
+                "Hey there new member, before you join our server I would like to go through some basic stuff with you.\n\n" +
                 "**🔒 Rules**\n\n" +
                 "Rules include but not limited to:\n\n" +
                 "`-` No NSFW, Advertising, Racist or Homophobic content\n" +
@@ -24,8 +24,8 @@ module.exports = {
                 "`-` No inappropriate links such as Ip loggers, Viruses or other links of that sort\n\n" +
                 "Refer to <#756197262320730152> for more detailed rules.\n\n" +
                 "**Note:** Server Admins have the authority to remove any user that doesn't present an acceptable behavior based on their judgement.\n\n" +
-                `If you have any questions feel free to dm <@${client.user.id}>\n\n` +
-                "Click the button bellow to verify that you agree with our rules, and that you feel ready to join our server"
+                `\`-\` **If you have any questions feel free to dm <@${client.user.id}>\n\n**` +
+                "**Click** the button bellow to verify that you agree with our rules, and that you feel ready to join our server"
             )
             .setColor(config.MainHexColor)
 
