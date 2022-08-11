4708b5a5a542248d51615f89d7f0a95e50f026b6
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-11 17:45:46 +0200

    Update

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -17,6 +17,7 @@ module.exports = {
             }
         } else if (interaction.isButton()) {
             require('./tickets/TicketOptions')(interaction, client)
+            require('./buttons/verify')(interaction, client)
         } else if (interaction.isSelectMenu()) {
             require('./menus/helpMenu')(interaction, client)
         }
