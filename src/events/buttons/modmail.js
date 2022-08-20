488cb443c5def40f686353c113ff0f772c5ac59f
Zofux <70540447+Zofux@users.noreply.github.com>
2022-08-20 14:12:32 +0200

    Update

--- src/events/buttons/modmail.js
+++ src/events/buttons/modmail.js
@@ -24,37 +24,37 @@ module.exports = async (interaction, client) => {
             }, {
                 upsert: true
             }).then(async () => {
-                await interaction.channel.delete().then(async () => {
-                    const logs = require('../../models/logChannels')
-                    const log = await logs.findOne({ guildId: interaction.guild.id })
-                    let doLog = false
-                    let logChannel;
-                    if (log) {
-                        doLog = true
-                    }
-                    if (doLog) {
-                        logChannel = interaction.guild.channels.cache.get(log.channelId)
-                        if (!logChannel) return;
-                        else if (logChannel) {
-                            const logEmbed = new Discord.MessageEmbed()
-                                .setAuthor(`ModMail Closed | ${interaction.channel.id}`)
-                                .setColor(config.MainHexColor)
-                                .addField("Channel", `\`${interaction.channel.id}\``)
-                                .addField("User", `\`${array[0].userId}\``)
-                                .addField("Moderator", `<@${interaction.user.id}>`)
-                            logChannel.send({ embeds: [logEmbed] })
-                        }
-                    }
 
-                    if (member) {
-                        const embed = new Discord.MessageEmbed()
-                            .setAuthor(client.user.username, client.user.displayAvatarURL())
-                            .setThumbnail(client.user.displayAvatarURL())
+                const logs = require('../../models/logChannels')
+                const log = await logs.findOne({ guildId: interaction.guild.id })
+                let doLog = false
+                let logChannel;
+                if (log) {
+                    doLog = true
+                }
+                if (doLog) {
+                    logChannel = interaction.guild.channels.cache.get(log.channelId)
+                    if (!logChannel) return;
+                    else if (logChannel) {
+                        const logEmbed = new Discord.MessageEmbed()
+                            .setAuthor(`ModMail Closed | ${interaction.channel.id}`)
                             .setColor(config.MainHexColor)
-                            .setDescription("Your current modmail has been closed by our staff team, simply send another message to me if you still need help")
-                        member.send({ embeds: [embed] })
+                            .addField("Channel", `\`${interaction.channel.id}\``)
+                            .addField("User", `\`${array[0].userId}\``)
+                            .addField("Moderator", `<@${interaction.user.id}>`)
+                        logChannel.send({ embeds: [logEmbed] })
                     }
-                })
+                }
+
+                if (member) {
+                    const embed = new Discord.MessageEmbed()
+                        .setAuthor(client.user.username, client.user.displayAvatarURL())
+                        .setThumbnail(client.user.displayAvatarURL())
+                        .setColor(config.MainHexColor)
+                        .setDescription("Your current modmail has been closed by our staff team, simply send another message to me if you still need help")
+                    member.send({ embeds: [embed] })
+                }
+                await interaction.channel.delete()
             })
         }
     }
