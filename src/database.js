7c1546790e16f9c4f1aadefac9a088abf9ec807a
Zofux <70540447+Zofux@users.noreply.github.com>
2023-04-14 14:23:27 +0200

    Update

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
-module.exports = () => {
+module.exports = async () => {
     const mongoose = require('mongoose')
-    mongoose.connect(process.env.database, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => console.log("Connected to the Database"))
+    await mongoose.connect(process.env.database || '', {
+        keepAlive: true,
+    })
+    if (mongoose.connect) {
+        console.log("Connected to Database")
+    }
 }
